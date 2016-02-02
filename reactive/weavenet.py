019174619912e3cb7c3ff9f5fa4be253e12f474a
Charles Butler <chuck@dasroot.net>
2016-02-02 16:16:10 +0100

    initial stab at cluster peering

--- reactive/weavenet.py
+++ reactive/weavenet.py
@@ -1,4 +1,5 @@
-from charms.reactive import when, when_not, set_state
+from charmhelpers.core.hookenv import leader_set, leader_get, unit_get
+from charms.reactive import when, when_not, set_state, hook
 from subprocess import check_output
 from shlex import split
 import os
@@ -10,6 +11,28 @@ def install_weavenet():
     _run("curl -sL https://git.io/weave -o /usr/local/bin/weave")
     os.chmod('/usr/local/bin/weave', 755)    
     set_state('weavenet.installed')
+
+@hook('leader-settings-changed')
+def join_weave_cluster():
+    ''' Followers will inherently run this hook, as the leader does not
+        execute a leader-settings-changed event, it only sends data, and its
+        a one way communication pipeline, like a broadcast'''
+    set_state('weavenet.reconfigure')
+
+@when('weavenet.reconfigure')
+def reconfigure_weavenet():
+    leader_address = leader_get('leader-address')
+    _run('weave connect {}'.format(leader_address))
+    remove_state('weavenet.reconfigure')
+
+
+
+@hook('leader-elected')
+def declare_weave_leader():
+    ''' Only the leader knows things, so send the leadership
+        address to any follower nodes so they can join in the party. 
+        wake me up before you go go go '''
+    leader_set('leader_address', unit_get('private-address'))
 
 @when('weavenet.installed')
 @when_not('weavenet.started')
