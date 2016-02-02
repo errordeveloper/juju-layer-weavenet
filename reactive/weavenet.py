58264184efc6f06ffbec5c82cb0923d1dd8308cc
Charles Butler <chuck@dasroot.net>
2016-02-02 15:37:27 +0100

    Getting started with stetting up weave

--- reactive/weavenet.py
+++ reactive/weavenet.py
@@ -1,7 +1,22 @@
 from charms.reactive import when, when_not, set_state
+from subprocess import check_output
+from shlex import split
+import os
 
 @when('docker.available')
-@when_not('weavenet.installed')
 def install_weavenet():
-    pass
+    # TODO: I will find a better way to do this shortly.
+    # todo - add a crypto verification
+    _run("curl -sL https://git.io/weave -o /usr/local/bin/weave")
+    os.chmod('/usr/local/bin/weave', 755)    
     set_state('weavenet.installed')
+
+@when('weavenet.installed')
+@when_not('weavenet.started')
+def start_weave():
+    _run("weave launch")
+    set_state('weavenet.started') 
+ 
+
+def _run(cmd):
+   return check_output(split(cmd))
